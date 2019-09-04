65e2492254cdee9901c78188384dc956ef0d0107
Blue1084 <laura@MacBook-Air-de-Laura.local>
2019-09-04 12:18:55 +0200

    las imagenes del franchise ya loopean

--- src/components/FranchiseCollections.vue
+++ src/components/FranchiseCollections.vue
@@ -1,13 +1,22 @@
 <template>
   <div>
-    <div v-for="franchise in franchises" :key="franchise" v-if="franchise">
+    <div
+      class="bodyfranchise"
+      v-for="(franchise,index) in franchises"
+      :key="franchise"
+      v-if="franchise"
+    >
       <!-- v-if='series' con esto le estamos diciendo que solo pinte las 
       que tengan contenido en serie. (si es null no los pintara).-->
-      <p>{{franchise}}</p>
+
       <router-link :to="'/Franchise/' + franchise.replace(' ', '')">
         <!-- remplace se utiliza para cambiar o remmplazar un elemento en este caso en concreto 
         lo utilizamos para quitar los espacios en el nombre de la serie-->
-        <img src="../assets/images_for_games.jpeg" />
+        <img
+          class="sizeimage"
+          :src="require('../assets/'+imagenFranchise(index)+'.png')"
+          :alt="franchise"
+        />
       </router-link>
     </div>
   </div>
@@ -15,9 +24,21 @@
 
 <script>
 export default {
+  data() {
+    return {
+      imageFranchise: []
+    };
+  },
+  methods: {
+    imagenFranchise(index) {
+      return this.franchises[index].replace(" ", "");
+    }
+  },
   computed: {
     franchises() {
-      return new Set(this.$store.getters.getamiiboStore.map(a => a.franchise));
+      return [
+        ...new Set(this.$store.getters.getamiiboStore.map(a => a.franchise))
+      ];
       //el map se utiliza para no repetir los elementos. crea un array  que contiene uno de cada elemento.
     }
     //siempre que necesitamos renderizar algo del store en el template lo ponemos en el computed
@@ -27,4 +48,18 @@ export default {
 </script>
 
 <style>
+.sizeimage {
+  height: auto;
+  width: 300px;
+  border-style: solid;
+  border-color: darkgray;
+  border-radius: 15px;
+  padding: 15px;
+}
+.bodyfranchise {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 55px;
+  margin-top: 25px;
+}
 </style>
